417ffc5d0a1989c2e85342dc8390569f2c3f96c2
Cristan Meijer <cristan.meijer@gmail.com>
2024-09-04 09:22:58 +0200

    Fix not always showing disruptions

--- app/src/main/java/nl/ovfietsbeschikbaarheid/mapper/DetailsMapper.kt
+++ app/src/main/java/nl/ovfietsbeschikbaarheid/mapper/DetailsMapper.kt
@@ -101,7 +101,7 @@ object DetailsMapper {
             serviceType = serviceType,
             directions = if (directions != "") directions else null,
             about = about,
-            disruptions = "Helaas kunt u hier op dit moment geen OV-fiets huren. Onze excuses voor dit ongemak. Raadpleeg de NS Reisplanner app of website (ns.nl/ov-fiets) voor de dichtstbijzijnde OV-fiets verhuurlocatie.",
+            disruptions = disruptions,
             location = location,
             coordinates = LatLng(payload.lat, payload.lng),
             stationName = allStations[payload.stationCode],
