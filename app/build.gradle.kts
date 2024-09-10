1fcabaf846d84e33b111402cd4595df6281afb70
Cristan Meijer <cristan.meijer@gmail.com>
2024-09-10 17:33:03 +0200

    Bump versionCode to 6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "nl.ovfietsbeschikbaarheid"
         minSdk = 26
         targetSdk = 34
-        versionCode = 5
+        versionCode = 6
         versionName = "1.0"
 
         // Only include resources for supported languages
