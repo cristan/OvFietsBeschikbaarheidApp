7c9b9be7e97f6141eabc40079f49526d6f35785e
Cristan Meijer <cristan.meijer@gmail.com>
2024-09-30 23:03:41 +0200

    Revert back to regular animations
    I just can't get it to work properly.

--- app/src/main/java/nl/ovfietsbeschikbaarheid/ui/navigation/Navigation.kt
+++ app/src/main/java/nl/ovfietsbeschikbaarheid/ui/navigation/Navigation.kt
@@ -1,12 +1,6 @@
 package nl.ovfietsbeschikbaarheid.ui.navigation
 
-import androidx.compose.animation.AnimatedContentScope
-import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.runtime.Composable
-import androidx.navigation.NamedNavArgument
-import androidx.navigation.NavBackStackEntry
-import androidx.navigation.NavDeepLink
-import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -25,18 +19,19 @@ fun Navigation() {
                 onLocationClick = { navController.navigate("detail/${it.locationCode}") }
             )
         }
-        slideInOutComposable("info") {
+        composable("info") {
             AboutScreen(onBackClicked = {
                 navController.navigate("home") {
                     popUpTo(0)
                 }
             })
         }
-        slideInOutComposable("detail/{locationCode}") { backStackEntry ->
+        composable("detail/{locationCode}") { backStackEntry ->
             val locationCode = backStackEntry.arguments?.getString("locationCode")!!
             DetailScreen(
                 locationCode = locationCode,
                 onAlternativeClicked = { alternative ->
+                    // Navigate with a fade effect when clicking on an alternative
                     navController.navigate("detail/${alternative.locationCode}")
                 },
                 onBackClicked = {
@@ -48,26 +43,3 @@ fun Navigation() {
         }
     }
 }
-
-private fun NavGraphBuilder.slideInOutComposable(
-    route: String,
-    arguments: List<NamedNavArgument> = emptyList(),
-    deepLinks: List<NavDeepLink> = emptyList(),
-    content: @Composable AnimatedContentScope.(NavBackStackEntry) -> Unit
-) =
-    composable(
-        route = route,
-        arguments = arguments,
-        deepLinks = deepLinks,
-        enterTransition = {
-            return@composable slideIntoContainer(
-                AnimatedContentTransitionScope.SlideDirection.Start
-            )
-        },
-        popExitTransition = {
-            return@composable slideOutOfContainer(
-                AnimatedContentTransitionScope.SlideDirection.End
-            )
-        },
-        content = content
-    )
