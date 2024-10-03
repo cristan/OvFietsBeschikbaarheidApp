c018a40a212ca43bb3873af54a4dd81ec0da3a64
Cristan Meijer <cristan.meijer@gmail.com>
2024-10-03 19:20:03 +0200

    Re-add animations
    And this time with a fade when navigating from detail to detail screen via the alternatives.

--- app/src/main/java/nl/ovfietsbeschikbaarheid/ui/navigation/Navigation.kt
+++ app/src/main/java/nl/ovfietsbeschikbaarheid/ui/navigation/Navigation.kt
@@ -1,6 +1,12 @@
 package nl.ovfietsbeschikbaarheid.ui.navigation
 
+import androidx.compose.animation.AnimatedContentScope
+import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.runtime.Composable
+import androidx.navigation.NamedNavArgument
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavDeepLink
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -19,19 +25,38 @@ fun Navigation() {
                 onLocationClick = { navController.navigate("detail/${it.locationCode}") }
             )
         }
-        composable("info") {
+        slideInOutComposable("info") {
             AboutScreen(onBackClicked = {
                 navController.navigate("home") {
                     popUpTo(0)
                 }
             })
         }
-        composable("detail/{locationCode}") { backStackEntry ->
+        composable(
+            route = "detail/{locationCode}",
+            enterTransition = {
+                return@composable if (this.initialState.destination.route == "detail/{locationCode}")
+                    null
+                else
+                    slideIntoContainer(AnimatedContentTransitionScope.SlideDirection.Start)
+            },
+            exitTransition = {
+                return@composable if (this.targetState.destination.route == "detail/{locationCode}")
+                    null
+                else
+                    slideOutOfContainer(AnimatedContentTransitionScope.SlideDirection.End)
+            },
+            popExitTransition = {
+                return@composable if (this.targetState.destination.route == "detail/{locationCode}")
+                    null
+                else
+                    slideOutOfContainer(AnimatedContentTransitionScope.SlideDirection.End)
+            }
+        ) { backStackEntry ->
             val locationCode = backStackEntry.arguments?.getString("locationCode")!!
             DetailScreen(
                 locationCode = locationCode,
                 onAlternativeClicked = { alternative ->
-                    // Navigate with a fade effect when clicking on an alternative
                     navController.navigate("detail/${alternative.locationCode}")
                 },
                 onBackClicked = {
@@ -43,3 +68,19 @@ fun Navigation() {
         }
     }
 }
+
+private fun NavGraphBuilder.slideInOutComposable(
+    route: String,
+    arguments: List<NamedNavArgument> = emptyList(),
+    deepLinks: List<NavDeepLink> = emptyList(),
+    content: @Composable AnimatedContentScope.(NavBackStackEntry) -> Unit
+) =
+    composable(
+        route = route,
+        arguments = arguments,
+        deepLinks = deepLinks,
+        enterTransition = { slideIntoContainer(AnimatedContentTransitionScope.SlideDirection.Start) },
+        popExitTransition = { slideOutOfContainer(AnimatedContentTransitionScope.SlideDirection.End) },
+        exitTransition = { slideOutOfContainer(AnimatedContentTransitionScope.SlideDirection.End) },
+        content = content
+    )
