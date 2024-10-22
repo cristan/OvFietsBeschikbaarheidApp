477f9eece39b48ca34bdf16e54bb09a4ac6e35d5
Cristan Meijer <cristan.meijer@gmail.com>
2024-10-22 20:49:04 +0200

    Bump version to 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "nl.ovfietsbeschikbaarheid"
         minSdk = 26
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.1"
+        versionCode = 11
+        versionName = "1.2"
 
         // Only include resources for supported languages
         resourceConfigurations += listOf("nl", "en")
