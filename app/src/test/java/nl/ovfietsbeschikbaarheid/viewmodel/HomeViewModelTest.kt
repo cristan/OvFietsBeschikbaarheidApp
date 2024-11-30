76e748d4eb7f0320b6b20dbce5f52df64889012e
Cristan Meijer <cristan.meijer@gmail.com>
2024-11-30 15:15:58 +0100

    Test the GPS isn't reloaded when showing the GPS twice in quick succession
    It's fine when you have a quick GPS, but it does take needless battery and can cause a needless slow app when the GPS isn't too quick.

--- app/src/test/java/nl/ovfietsbeschikbaarheid/viewmodel/HomeViewModelTest.kt
+++ app/src/test/java/nl/ovfietsbeschikbaarheid/viewmodel/HomeViewModelTest.kt
@@ -325,6 +325,24 @@ class HomeViewModelTest {
         viewModelContent.locations[0].location.rentalBikesAvailable shouldBeEqualTo 1337
     }
 
+    @Test
+    fun `gps isn't refreshed again when going to the GPS list again right away`() = runTest {
+        launchWithEverythingOk()
+
+        assertIs<HomeContent.GpsContent>(viewModel.content.value)
+
+        coVerify(exactly = 1) { locationLoader.loadCurrentCoordinates() }
+
+        coEvery { findNearbyLocationsUseCase.invoke(any(), any()) } returns null // We're not testing nearby locations here
+
+        viewModel.onSearchTermChanged("a")
+        viewModel.onSearchTermChanged("")
+
+        assertIs<HomeContent.GpsContent>(viewModel.content.value)
+
+        coVerify(exactly = 1) { locationLoader.loadCurrentCoordinates() }
+    }
+
     private fun launchWithEverythingOk(allLocations: List<LocationOverviewModel> = listOf(TestData.testLocationOverviewModel)) {
         stubLocationsOk(allLocations)
         stubGpsOk()
