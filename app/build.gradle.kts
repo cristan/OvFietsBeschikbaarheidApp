46fe9e0293d7f5710adc42950e37e1705dc0adbd
Cristan Meijer <cristan.meijer@gmail.com>
2025-05-24 12:07:02 +0200

    Bump version to 2.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "nl.ovfietsbeschikbaarheid"
         minSdk = 26
         targetSdk = 35
-        versionCode = 18
-        versionName = "2.4.0"
+        versionCode = 19
+        versionName = "2.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
