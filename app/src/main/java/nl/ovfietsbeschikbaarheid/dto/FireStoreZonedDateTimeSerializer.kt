781451719cb625c80aef0d0ef2c0ba691f82be5b
Cristan Meijer <cristan.meijer@gmail.com>
2025-07-26 22:25:31 +0200

    Support parsing all firebase date formats

--- app/src/main/java/nl/ovfietsbeschikbaarheid/dto/FireStoreZonedDateTimeSerializer.kt
+++ app/src/main/java/nl/ovfietsbeschikbaarheid/dto/FireStoreZonedDateTimeSerializer.kt
@@ -8,18 +8,28 @@ import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeFormatterBuilder
+import java.time.temporal.ChronoField
 
 class FireStoreZonedDateTimeSerializer : KSerializer<ZonedDateTime> {
-    private val dateTimePattern: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSSX")
+    // Firebase can return different amounts of fractional second digits
+    // 2025-07-18T22:00:53.145887Z
+    // 2025-07-24T19:00:11.332Z
+
+    private val flexibleFormatter: DateTimeFormatter = DateTimeFormatterBuilder()
+        .appendPattern("yyyy-MM-dd'T'HH:mm:ss")
+        .appendFraction(ChronoField.NANO_OF_SECOND, 0, 6, true) // optional .SSS to .SSSSSS
+        .appendOffsetId()
+        .toFormatter()
 
     override val descriptor: SerialDescriptor =
-        PrimitiveSerialDescriptor("com.ovfietsbeschikbaarheid.BigDecimalSerializer", PrimitiveKind.STRING)
+        PrimitiveSerialDescriptor("com.ovfietsbeschikbaarheid.FireStoreZonedDateTimeSerializer", PrimitiveKind.STRING)
 
     override fun serialize(encoder: Encoder, value: ZonedDateTime) {
-        encoder.encodeString(dateTimePattern.format(value))
+        encoder.encodeString(flexibleFormatter.format(value))
     }
 
     override fun deserialize(decoder: Decoder): ZonedDateTime {
-        return ZonedDateTime.parse(decoder.decodeString(), dateTimePattern)
+        return ZonedDateTime.parse(decoder.decodeString(), flexibleFormatter)
     }
 }
