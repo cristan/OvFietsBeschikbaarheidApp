a1d7cebe0ff5d7bb62f133fe6a3979699f15684d
Cristan Meijer <cristan.meijer@gmail.com>
2025-10-12 15:21:25 +0200

    Remove proguard-rules.pro
    We apparently no longer need this, so let's get rid of it.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,7 @@ android {
         release {
             isMinifyEnabled = true
             proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                getDefaultProguardFile("proguard-android-optimize.txt")
             )
         }
     }
