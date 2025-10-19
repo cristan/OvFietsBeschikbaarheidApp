142d390260985aab952a5e9847c5e52f432a4480
Cristan Meijer <cristan.meijer@gmail.com>
2025-10-19 20:50:25 +0200

    Init Koin in iOS as well
    No idea why it only seems to work in MainViewController.kt, but I'm happy that it does.

--- composeApp/src/iosMain/kotlin/nl/ovfietsbeschikbaarheid/ui/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/nl/ovfietsbeschikbaarheid/ui/MainViewController.kt
@@ -1,6 +1,16 @@
 package nl.ovfietsbeschikbaarheid.ui
 
 import androidx.compose.ui.window.ComposeUIViewController
+import nl.ovfietsbeschikbaarheid.di.commonModule
+import nl.ovfietsbeschikbaarheid.di.iosModule
 import nl.ovfietsbeschikbaarheid.ui.screen.AboutScreen
+import org.koin.core.context.startKoin
 
 fun MainViewController() = ComposeUIViewController { AboutScreen(pricePer24Hours = null, onBackClicked = {}) }
+
+fun doInitKoin() = startKoin {
+    modules(
+        commonModule(),
+        iosModule()
+    )
+}
--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,7 +1,12 @@
 import SwiftUI
+import ComposeApp
 
 @main
 struct iOSApp: App {
+    init() {
+        MainViewControllerKt.doInitKoin()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
