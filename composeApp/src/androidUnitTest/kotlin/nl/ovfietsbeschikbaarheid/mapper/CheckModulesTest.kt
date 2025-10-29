70bf534cf1ab7d2e94f212f1603006db7b9beb77
Cristan Meijer <cristan.meijer@gmail.com>
2025-10-29 21:40:36 +0100

    Fix CheckModulesTest

--- composeApp/src/androidUnitTest/kotlin/nl/ovfietsbeschikbaarheid/mapper/CheckModulesTest.kt
+++ composeApp/src/androidUnitTest/kotlin/nl/ovfietsbeschikbaarheid/mapper/CheckModulesTest.kt
@@ -3,20 +3,31 @@ package nl.ovfietsbeschikbaarheid.mapper
 import androidx.datastore.core.DataStore
 import nl.ovfietsbeschikbaarheid.di.commonModule
 import nl.ovfietsbeschikbaarheid.util.AndroidLocationLoader
+import nl.ovfietsbeschikbaarheid.util.DecimalFormatter
 import nl.ovfietsbeschikbaarheid.util.InAppReviewProvider
 import nl.ovfietsbeschikbaarheid.util.LocationLoader
 import nl.ovfietsbeschikbaarheid.util.LocationPermissionHelper
 import org.junit.Test
+import org.koin.core.annotation.KoinExperimentalAPI
 import org.koin.test.KoinTest
 import org.koin.test.verify.verify
 
 class CheckModulesTest : KoinTest {
 
+    @OptIn(KoinExperimentalAPI::class)
     @Test
     fun `check all modules`() {
-        // These classes come from the androidModule
-        commonModule().verify(extraTypes =
-            listOf(DataStore::class, InAppReviewProvider::class, LocationPermissionHelper::class, AndroidLocationLoader::class, LocationLoader::class)
+        commonModule().verify(
+            // These classes come from the androidModule
+            extraTypes =
+                listOf(
+                    DataStore::class,
+                    InAppReviewProvider::class,
+                    LocationPermissionHelper::class,
+                    AndroidLocationLoader::class,
+                    LocationLoader::class,
+                    DecimalFormatter::class
+                )
         )
     }
 }
