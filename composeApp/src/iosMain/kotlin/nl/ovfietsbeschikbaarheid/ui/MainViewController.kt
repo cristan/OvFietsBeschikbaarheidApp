b6dbd4c552c58cb62092651d811faa75b653054f
Cristan Meijer <cristan.meijer@gmail.com>
2025-12-07 13:07:07 +0100

    Use parallel rendering on iOS
    This should improve performance. I don't notice any graphical issues, so I think it's safe to turn on.

--- composeApp/src/iosMain/kotlin/nl/ovfietsbeschikbaarheid/ui/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/nl/ovfietsbeschikbaarheid/ui/MainViewController.kt
@@ -1,12 +1,14 @@
 package nl.ovfietsbeschikbaarheid.ui
 
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeUIViewController
 import nl.ovfietsbeschikbaarheid.di.commonModule
 import nl.ovfietsbeschikbaarheid.di.iosModule
 import nl.ovfietsbeschikbaarheid.ui.navigation.Navigation
 import org.koin.core.context.startKoin
 
-fun MainViewController() = ComposeUIViewController { Navigation() }
+@OptIn(ExperimentalComposeUiApi::class)
+fun MainViewController() = ComposeUIViewController(configure = { parallelRendering = true }) { Navigation() }
 
 fun doInitKoin() = startKoin {
     modules(
