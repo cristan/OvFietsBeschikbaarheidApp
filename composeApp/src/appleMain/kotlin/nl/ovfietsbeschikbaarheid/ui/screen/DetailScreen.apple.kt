cca36e6df88cb11d52cfcde0d64bc96f936d0ba2
Cristan Meijer <cristan.meijer@gmail.com>
2025-12-08 14:20:41 +0100

    Fix the navigate button not doing anything on iOS 18 and up
    Thanks people from #DutchAUG!

--- composeApp/src/appleMain/kotlin/nl/ovfietsbeschikbaarheid/ui/screen/DetailScreen.apple.kt
+++ composeApp/src/appleMain/kotlin/nl/ovfietsbeschikbaarheid/ui/screen/DetailScreen.apple.kt
@@ -1,15 +1,17 @@
 package nl.ovfietsbeschikbaarheid.ui.screen
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalUriHandler
 import io.ktor.http.URLBuilder
-import platform.Foundation.NSURL
-import platform.UIKit.UIApplication
 
 @Composable
-actual fun onLocationClicked(): (String) -> Unit = { address ->
-    val url = URLBuilder("http://maps.apple.com/").apply {
-        parameters.append("q", address)
-    }.build()
-    val nsUrl = NSURL.URLWithString(url.toString())!!
-    UIApplication.sharedApplication.openURL(nsUrl)
+actual fun onLocationClicked(): (String) -> Unit {
+    val uriHandler = LocalUriHandler.current
+    val onLocationClicked: (String) -> Unit = { address ->
+        val url = URLBuilder("http://maps.apple.com/").apply {
+            parameters.append("q", address)
+        }.build()
+        uriHandler.openUri(url.toString())
+    }
+    return onLocationClicked
 }
