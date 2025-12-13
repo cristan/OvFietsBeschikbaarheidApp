eff56828c3b9e9fde5daf9f5168926c6a6526bad
Cristan Meijer <cristan.meijer@gmail.com>
2025-12-13 21:45:08 +0100

    Make the details skeleton screens more realistic on tablet

--- composeApp/src/commonMain/kotlin/nl/ovfietsbeschikbaarheid/ui/screen/DetailScreen.kt
+++ composeApp/src/commonMain/kotlin/nl/ovfietsbeschikbaarheid/ui/screen/DetailScreen.kt
@@ -57,6 +57,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.valentinilk.shimmer.Shimmer
 import com.valentinilk.shimmer.ShimmerBounds
 import com.valentinilk.shimmer.rememberShimmer
 import com.valentinilk.shimmer.shimmer
@@ -225,78 +226,128 @@ fun DetailsLoader(
     modifier: Modifier
 ) {
     val shimmerInstance = rememberShimmer(shimmerBounds = ShimmerBounds.Window)
+    val spacing = LocalSpacing.current
     Column(
         modifier
-            .padding(top = 4.dp, start = 20.dp, end = 20.dp)
+            .padding(start = spacing, end = spacing)
             .verticalScroll(rememberScrollState())
     ) {
-        OvCard {
-            Text(stringResource(Res.string.details_amount_available))
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 24.dp),
-                contentAlignment = Alignment.Center
-            ) {
-                CircularProgressIndicator(
-                    progress = { 1.0f },
-                    color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.4f),
-                    modifier = Modifier
-                        .size(220.dp)
-                        .shimmer(shimmerInstance),
-                    strokeWidth = 36.dp,
-                    strokeCap = StrokeCap.Butt,
-                    gapSize = 0.dp,
+        val windowSizeClass = currentWindowAdaptiveInfo().windowSizeClass
+        val tabletSized = windowSizeClass.isWidthAtLeastBreakpoint(800)
+        if (tabletSized) {
+            Row {
+                Column(modifier = Modifier.weight(1f)) {
+                    LoadingGauge(shimmerInstance)
 
-                    )
-                Box(
-                    modifier = Modifier
-                        .size(width = 68.dp, height = 54.dp)
-                        .shimmerShape(shimmerInstance)
-                )
+                        OvCard {
+                            Text(
+                                text = stringResource(Res.string.opening_hours_title),
+                                style = MaterialTheme.typography.headlineMedium,
+                                modifier = Modifier.padding(bottom = 8.dp)
+                            )
+                            Box(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(180.dp)
+                                    .shimmerShape(shimmerInstance)
+                            )
+                        }
+                }
+                Spacer(Modifier.width(spacing))
+                Column(modifier = Modifier.weight(2f)) {
+                    LoadingGraph(shimmerInstance)
+
+                    LoadingMap(shimmerInstance)
+                }
             }
-            Row(Modifier.align(Alignment.End)) {
-                Text(
-                    stringResource(Res.string.open_until, "23:33"),
-                    modifier = Modifier.shimmerShape(shimmerInstance)
-                )
-            }
+        } else {
+            LoadingGauge(shimmerInstance)
+
+            LoadingGraph(shimmerInstance)
+
+            LoadingMap(shimmerInstance)
         }
 
-        OvCard {
-            Text(
-                text = stringResource(Res.string.capacity_graph_title),
-                style = MaterialTheme.typography.headlineMedium,
-                modifier = Modifier.padding(bottom = 16.dp)
-            )
+        Spacer(
+            Modifier.windowInsetsBottomHeight(WindowInsets.systemBars)
+        )
+    }
+}
 
+@Composable
+private fun LoadingMap(shimmerInstance: Shimmer) {
+    OvCard {
+        Text(
+            text = stringResource(Res.string.location_title),
+            style = MaterialTheme.typography.headlineMedium,
+        )
+
+        Box(
+            modifier = Modifier
+                .padding(vertical = 32.dp)
+                .height(28.dp)
+                .fillMaxWidth()
+                .shimmerShape(shimmerInstance, shape = RoundedCornerShape(8.dp))
+        )
+
+        Box(
+            modifier = Modifier
+                .height(276.dp)// 260 dp + 16 dp padding. Not sure where that 16 dp comes from, but ok.
+                .fillMaxWidth()
+                .shimmerShape(shimmerInstance, shape = RoundedCornerShape(12.dp))
+        )
+    }
+}
+
+@Composable
+private fun LoadingGraph(shimmerInstance: Shimmer, boxHeight: Dp = 370.dp) {
+    OvCard {
+        Text(
+            text = stringResource(Res.string.capacity_graph_title),
+            style = MaterialTheme.typography.headlineMedium,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+
+        Box(
+            modifier = Modifier
+                .height(boxHeight)
+                .fillMaxWidth()
+                .shimmerShape(shimmerInstance, shape = RoundedCornerShape(8.dp))
+        )
+    }
+}
+
+@Composable
+private fun LoadingGauge(shimmerInstance: Shimmer) {
+    OvCard {
+        Text(stringResource(Res.string.details_amount_available))
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 24.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator(
+                progress = { 1.0f },
+                color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.4f),
+                modifier = Modifier
+                    .size(220.dp)
+                    .shimmer(shimmerInstance),
+                strokeWidth = 36.dp,
+                strokeCap = StrokeCap.Butt,
+                gapSize = 0.dp,
+
+                )
             Box(
                 modifier = Modifier
-                    .height(270.dp)
-                    .fillMaxWidth()
-                    .shimmerShape(shimmerInstance, shape = RoundedCornerShape(8.dp))
+                    .size(width = 68.dp, height = 54.dp)
+                    .shimmerShape(shimmerInstance)
             )
         }
-
-        OvCard {
+        Row(Modifier.align(Alignment.End)) {
             Text(
-                text = stringResource(Res.string.location_title),
-                style = MaterialTheme.typography.headlineMedium,
-            )
-
-            Box(
-                modifier = Modifier
-                    .padding(vertical = 32.dp)
-                    .height(28.dp)
-                    .fillMaxWidth()
-                    .shimmerShape(shimmerInstance, shape = RoundedCornerShape(8.dp))
-            )
-
-            Box(
-                modifier = Modifier
-                    .height(276.dp)// 260 dp + 16 dp padding. Not sure where that 16 dp comes from, but ok.
-                    .fillMaxWidth()
-                    .shimmerShape(shimmerInstance, shape = RoundedCornerShape(12.dp))
+                stringResource(Res.string.open_until, "23:33"),
+                modifier = Modifier.shimmerShape(shimmerInstance)
             )
         }
     }
@@ -319,9 +370,6 @@ private fun ActualDetails(
                 Row {
                     Column(modifier = Modifier.weight(1f)) {
                         Gauge(details)
-                        details.disruptions?.let {
-                            Disruptions(it)
-                        }
                         details.disruptions?.let {
                             Disruptions(it)
                         }
@@ -491,7 +539,7 @@ fun MapView(
     directions: String?,
     description: String,
     rentalBikesAvailable: Int?,
-    mapHeight: Dp ,
+    mapHeight: Dp,
     onNavigateClicked: (String) -> Unit
 ) {
     OvCard {
@@ -665,6 +713,7 @@ fun DetailsLoadingPreview() {
 
 @OptIn(ExperimentalTime::class)
 @Preview(heightDp = 2000)
+@Preview(widthDp = 1483, heightDp = 928)
 @Composable
 fun DetailsPreview() {
     val dayNames =
