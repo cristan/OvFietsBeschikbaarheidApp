00e6013b7b54bc1b3afc012c8c03f666494cbdf8
Cristan Meijer <cristan.meijer@gmail.com>
2025-12-14 11:49:14 +0100

    Bump version to 3.6.0

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -115,8 +115,8 @@ android {
         applicationId = "nl.ovfietsbeschikbaarheid"
         minSdk = 26
         targetSdk = 36
-        versionCode = 26
-        versionName = "3.5.0"
+        versionCode = 27
+        versionName = "3.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
