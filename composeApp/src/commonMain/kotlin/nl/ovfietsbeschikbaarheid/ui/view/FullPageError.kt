dff37376ea6f57d4911b95a8e1f3bf5407b98634
Cristan Meijer <cristan.meijer@gmail.com>
2025-12-22 13:44:37 +0100

    Limit the size of the retry button to 600.dp

--- composeApp/src/commonMain/kotlin/nl/ovfietsbeschikbaarheid/ui/view/FullPageError.kt
+++ composeApp/src/commonMain/kotlin/nl/ovfietsbeschikbaarheid/ui/view/FullPageError.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -30,6 +32,8 @@ fun FullPageError(
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .wrapContentWidth(Alignment.CenterHorizontally)
+            .widthIn(max = 600.dp)
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
